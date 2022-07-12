5bcd2ab1640f393a2bf28d8c4703b53deeb4c20e
tenpei-sano <kikichan.fps@gmail.com>
2022-07-12 19:57:27 +0900

    マイグレーション修正

--- src/database/migrations/2022_07_08_143042_create_timecards_table.php
+++ src/database/migrations/2022_07_08_143042_create_timecards_table.php
@@ -14,15 +14,19 @@ class CreateTimecardsTable extends Migration
     public function up()
     {
         Schema::create('timecards', function (Blueprint $table) {
-            $table->unsignedBigInteger('dateTimeId')->comment('20200607など');
-            $table->foreignId('user_project_id')->references('id')->on('users_projects');
-            $table->integer('order')->comment('同日に複数チケットを選択したときを順番で区別');
-            $table->integer('ticketNumber')->comment('チケットNO');
-            $table->time('startTime')->comment('開始時間');
-            $table->time('endTime')->comment('終了時間');
-            $table->integer('breakTime')->comment('休憩時間、分表示？？');
-            $table->integer('otherCost')->comment('経費');
-            $table->string('costDetail')->comment('経費の備考');
+            $table->unsignedBigInteger('id')->comment('updateOrCreateするため2020060701など');
+            $table->foreignId('user_id');
+            $table->foreignId('project_id');
+            $table->string('user_name')->comment('ユーザーの名前');
+            $table->string('project_name')->comment('プロジェクトの名前');
+            $table->date('year_month')->comment('2022-06など');
+            $table->integer('day')->comment('2など');
+            $table->time('start_time')->comment('12:00など');
+            $table->time('finish_time')->comment('12:00など');
+            $table->time('rest_time')->comment('12:00など');
+            $table->integer('working_time')->comment('1など');
+            $table->integer('expenses')->comment('経費');
+            $table->string('remark')->comment('備考');
             $table->timestamps();
         });
     }
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -16,11 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([UsersTableSeeder::class]);
-        $this->call([ProjectsTableSeeder::class]);
-        $this->call([UsersProjectsTableSeeder::class]);
-        $this->call([BillsTableSeeder::class]);
-        $this->call([ContractsTableSeeder::class]);
-        $this->call([TimecardsTableSeeder::class]);
-        $this->call([Unit_pricesTableSeeder::class]);
+        // $this->call([ProjectsTableSeeder::class]);
+        // $this->call([UsersProjectsTableSeeder::class]);
+        // $this->call([BillsTableSeeder::class]);
+        // $this->call([TimecardsTableSeeder::class]);
     }
 }
