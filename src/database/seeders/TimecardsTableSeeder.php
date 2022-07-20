b8db38145ef0c8171617b00fc79088a13c66bb29
yuman211 <yukinuma0211@gmail.com>
2022-07-20 19:30:20 +0900

    stashのテスト

--- src/database/seeders/TimecardsTableSeeder.php
+++ src/database/seeders/TimecardsTableSeeder.php
@@ -18,7 +18,7 @@ class TimecardsTableSeeder extends Seeder
             [
                 'order' => 1,
                 'project_user_id' => 1,
-                'year_month' => 202207,
+                // 'year_month' => 202207,
                 'year_month_date' => "2022-07-11",
                 'ticket_number' => 2,
                 'ticket_name' => 'コロコロAPI実装',
@@ -31,7 +31,7 @@ class TimecardsTableSeeder extends Seeder
             [
                 'order' => 1,
                 'project_user_id' => 1,
-                'year_month' => 202207,
+                // 'year_month' => 202207,
                 'year_month_date' => "2022-07-12",
                 'ticket_number' => '2',
                 'ticket_name' => 'ピロピロ実装',
@@ -44,7 +44,7 @@ class TimecardsTableSeeder extends Seeder
             [
                 'order' => 1,
                 'project_user_id' => 1,
-                'year_month' => 202207,
+                // 'year_month' => 202207,
                 'year_month_date' => "2022-07-13",
                 'ticket_number' => '2',
                 'ticket_name' => 'ピロピロ実装',
@@ -57,7 +57,7 @@ class TimecardsTableSeeder extends Seeder
             [
                 'order' => 2,
                 'project_user_id' => 1,
-                'year_month' => 202207,
+                // 'year_month' => 202207,
                 'year_month_date' => "2022-07-13",
                 'ticket_number' => '4',
                 'ticket_name' => '鶏肉調理',
@@ -70,7 +70,7 @@ class TimecardsTableSeeder extends Seeder
             [
                 'order' => 1,
                 'project_user_id' => 2,
-                'year_month' => 202207,
+                // 'year_month' => 202207,
                 'year_month_date' => "2022-07-15",
                 'ticket_number' => '4',
                 'ticket_name' => '鶏肉調理',
@@ -83,7 +83,7 @@ class TimecardsTableSeeder extends Seeder
             [
                 'order' => 1,
                 'project_user_id' => 3,
-                'year_month' => 202207,
+                // 'year_month' => 202207,
                 'year_month_date' => "2022-07-16",
                 'ticket_number' => '6',
                 'ticket_name' => '鶏肉調理',
@@ -96,7 +96,7 @@ class TimecardsTableSeeder extends Seeder
             [
                 'order' => 1,
                 'project_user_id' => 4,
-                'year_month' => 202207,
+                // 'year_month' => 202207,
                 'year_month_date' => "2022-07-16",
                 'ticket_number' => '7',
                 'ticket_name' => 'ぶた肉調理',
@@ -109,7 +109,7 @@ class TimecardsTableSeeder extends Seeder
             [
                 'order' => 1,
                 'project_user_id' => 3,
-                'year_month' => 202207,
+                // 'year_month' => 202207,
                 'year_month_date' => "2022-07-19",
                 'ticket_number' => '7',
                 'ticket_name' => '肉調理',
@@ -122,7 +122,7 @@ class TimecardsTableSeeder extends Seeder
             [
                 'order' => 1,
                 'project_user_id' => 3,
-                'year_month' => 202208,
+                // 'year_month' => 202208,
                 'year_month_date' => "2022-08-19",
                 'ticket_number' => '7',
                 'ticket_name' => '肉調理',
